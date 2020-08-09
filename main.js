b110dddbb135290aba0d64e6afef03827db93be8
Chris LoJacono <clojacono92@gmail.com>
2020-08-09 12:52:13 -0500

    added the shop cards array of objects layout with one actual product

---
+++ main.js
@@ -0,0 +1,39 @@
+const shopCardsArray = [
+  {
+    style: "Real Teal",
+    forWhom: "Men",
+    onSale: false,
+    description:
+      "DESIGNED WITH 5-POCKET DENIM STYLING, DETAILS INCLUDE A ZIP FLY WITH METAL BUTTON CLOSURE, COIN POCKET AT THE LEFT HIP AND RVCA METAL RIVETS AND THE POCKETS.",
+    imageUrl: "/images/realteal.jpg",
+    price: "60.00",
+  },
+  {
+    style: "Real Teal",
+    forWhom: "Men",
+    onSale: false,
+    description:
+      "DESIGNED WITH 5-POCKET DENIM STYLING, DETAILS INCLUDE A ZIP FLY WITH METAL BUTTON CLOSURE, COIN POCKET AT THE LEFT HIP AND RVCA METAL RIVETS AND THE POCKETS.",
+    imageUrl: "/images/realteal.jpg",
+    price: "60.00",
+  },
+  {
+    style: "Real Teal",
+    forWhom: "Men",
+    onSale: false,
+    description:
+      "DESIGNED WITH 5-POCKET DENIM STYLING, DETAILS INCLUDE A ZIP FLY WITH METAL BUTTON CLOSURE, COIN POCKET AT THE LEFT HIP AND RVCA METAL RIVETS AND THE POCKETS.",
+    imageUrl: "/images/realteal.jpg",
+    price: "60.00",
+  },
+  {
+    style: "Real Teal",
+    forWhom: "Men",
+    onSale: false,
+    description:
+      "DESIGNED WITH 5-POCKET DENIM STYLING, DETAILS INCLUDE A ZIP FLY WITH METAL BUTTON CLOSURE, COIN POCKET AT THE LEFT HIP AND RVCA METAL RIVETS AND THE POCKETS.",
+    imageUrl: "/images/realteal.jpg",
+    price: "60.00",
+  },
+];
+
