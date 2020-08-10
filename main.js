5a75502528bd4d9b56aedc1fab22e35d4373e0d9
Will Kotheimer <wkotheimer@gmail.com>
2020-08-10 13:47:12 -0500

    Added Reviews Array

--- main.js
+++ main.js
@@ -1,11 +1,41 @@
 const printToDom = (divID, textToPrint) => {
-    const selectedDiv = document.getElementById(divID);
-    selectedDiv.innerHTML = textToPrint;
-  };
+  const selectedDiv = document.getElementById(divID);
+  selectedDiv.innerHTML = textToPrint;
+};
 
-  
-const renderNavbar = ()=>{
-    domString = `<nav class="navbar navbar-expand-lg navbar-light bg-light">
+const reviewsArray = [
+  {
+    "name": "Alexander Solzenezen",
+    "product": "classic pants",
+    "rating": "5",
+    "product": "Corduroys",
+    "comment": "I love them."
+  },
+  {
+    "name": "John Steinback",
+    "product": "classic pants",
+    "rating": "3",
+    "product": "Corduroys",
+    "comment": "Could've been better."
+  },
+  {
+    "name": "William Faulkner",
+    "product": "classic pants",
+    "rating": "2",
+    "product": "Corduroys",
+    "comment": "I would've given three stars but the service wasn't good either."
+  },
+  {
+    "name": "Djuna Barnes",
+    "product": "classic pants",
+    "rating": "5",
+    "product": "Corduroys",
+    "comment": "Best I ever got."
+  }
+];
+
+const renderNavbar = () => {
+  domString = `<nav class="navbar navbar-expand-lg navbar-light bg-light">
     <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
       <span class="navbar-toggler-icon"></span>
     </button>
@@ -24,8 +54,8 @@ const renderNavbar = ()=>{
 
 }
 
-const init =()=>{
-    renderNavbar();
+const init = () => {
+  renderNavbar();
 }
 
 init();
